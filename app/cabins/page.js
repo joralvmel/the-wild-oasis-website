dee1641d6f21d4f1f631377fb921d48b8eef5052
joralvmel <joralvmel@gmail.com>
2024-08-06 09:59:45 +0200

    Display loading indicator

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,7 +4,6 @@ export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
-  console.log(data);
   return (
     <div>
       <h1>Cabins Page</h1>
